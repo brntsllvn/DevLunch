e46b14c33770854a77e6f49414a97c35b14a7897
Josh Rolstad <jrolstad@gmail.com>
2016-04-26 13:59:19 -0700

    Adding test project

---
+++ GeneralStore.Tests/Controllers/api/ProductControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using GeneralStore.Controllers.api;
+using NUnit.Framework;
+
+namespace GeneralStore.Tests.Controllers.api
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        [Test]
+        public void Get_NoParameters_ReturnsAllProduct()
+        {
+            // Arrange
+            var controller = new ProductController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
