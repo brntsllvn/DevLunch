80f60928c975c3147ebbb0209ce0e7378a6172f9
Brent Sullivan <brntsllvn@gmail.com>
2016-05-02 09:02:03 -0700

    add mvc Products controller

---
+++ GeneralStore.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using GeneralStore.Controllers;
+using GeneralStore.Data.Models;
+using NUnit.Framework;
+using Shouldly;
+
+namespace GeneralStore.Tests.Controllers
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        [Test]
+        public void Index_NoParameters_ReturnsAllProducts()
+        {
+            // Arrange
+            var controller = new ProductController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            result.Model.ShouldNotBeNull();
+            var data = result.Model as IEnumerable<Product>;
+            data.ShouldNotBeEmpty();
+        }
+    }
+}
---
+++ GeneralStore/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeneralStore.Data.Models;
+
+namespace GeneralStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ViewResult Index()
+        {
+            return View(new List<Product> { new Product() } );
+        }
+    }
+}
