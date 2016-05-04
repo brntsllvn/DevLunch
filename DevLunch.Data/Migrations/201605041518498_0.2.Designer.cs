0a5366cbdaa762d4aa3b7b3768cfd05a2c59f31b
Brent Sullivan <brntsllvn@gmail.com>
2016-05-04 08:23:46 -0700

    Updating database to use Restaurants

--- DevLunch.Data/Migrations/201605041518498_0.2.Designer.cs
+++ DevLunch.Data/Migrations/201605041518498_0.2.Designer.cs
@@ -7,13 +7,13 @@ namespace DevLunch.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class _01 : IMigrationMetadata
+    public sealed partial class _02 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(_01));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(_02));
         
         string IMigrationMetadata.Id
         {
-            get { return "201604262129146_0.1"; }
+            get { return "201605041518498_0.2"; }
         }
         
         string IMigrationMetadata.Source
--- DevLunch.Data/Migrations/201605041518498_0.2.cs
+++ DevLunch.Data/Migrations/201605041518498_0.2.cs
@@ -3,7 +3,7 @@ namespace DevLunch.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class _01 : DbMigration
+    public partial class _02 : DbMigration
     {
         public override void Up()
         {
@@ -16,10 +16,22 @@ namespace DevLunch.Data.Migrations
                     })
                 .PrimaryKey(t => t.Id);
             
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Latitude = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Longitude = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
         }
         
         public override void Down()
         {
+            DropTable("dbo.Restaurants");
             DropTable("dbo.Products");
         }
     }
