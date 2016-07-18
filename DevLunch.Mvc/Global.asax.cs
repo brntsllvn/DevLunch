1e7a0cc88c21a526da3e832e6c39a489ab580142
Brent Sullivan <brntsllvn@gmail.com>
2016-07-18 08:44:00 -0700

    add log4net

--- DevLunch.Mvc/Global.asax.cs
+++ DevLunch.Mvc/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net.Config;
 
 namespace DevLunch.Mvc
 {
@@ -13,6 +14,8 @@ namespace DevLunch.Mvc
     {
         protected void Application_Start()
         {
+            XmlConfigurator.Configure();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
